bf5da085503a767fde037f4278dfaacc5b6ab866
hafidhadhiet <46853809+hafidhadhiet@users.noreply.github.com>
2021-06-11 02:13:40 +0700

    kelar

--- hasil_tpot.py
+++ hasil_tpot.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+from xgboost import XGBClassifier
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +13,14 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.854092514982846
-exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=9, max_features=0.8, min_samples_leaf=17, min_samples_split=10, n_estimators=100, subsample=1.0)
+# Average CV score on the training set was: 0.8551841772997439
+exported_pipeline = make_pipeline(
+    make_union(
+        FunctionTransformer(copy),
+        FunctionTransformer(copy)
+    ),
+    XGBClassifier(learning_rate=0.1, max_depth=8, min_child_weight=12, n_estimators=100, n_jobs=1, subsample=0.9500000000000001, verbosity=0)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
